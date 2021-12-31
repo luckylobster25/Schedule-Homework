bbf55f5684a14dbee7da71791263d024f33fd5f1
Chue Chang <chuechang1996@outlook.com>
2021-12-31 16:32:52 -0600

    saving to local and retreating it

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ setInterval(function(){
     var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     $("#currentDay").text(currentDay)
 },0) // time is set to 0 so current day is display right away. 
-
 function save() {
-    localStorage.setItem()
-}
+    localStorage.setItem(document.getElementById("slot-one").name, document.getElementById("slot-one").value)
+} 
+var saveData = localStorage.getItem("9am")
+$("#slot-one").text(saveData)
