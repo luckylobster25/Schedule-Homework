1106d85e5e2d53d15a7d7fccf31a2f74d1c9035d
Chue Chang <chuechang1996@outlook.com>
2021-12-31 16:11:27 -0600

    style container to complete look

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 setInterval(function(){
     var currentDay = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
     $("#currentDay").text(currentDay)
-},0)
+},0) // time is set to 0 so current day is display right away. 
+
+function save() {
+    localStorage.setItem()
+}
